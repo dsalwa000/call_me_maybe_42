359e1f93bbfb27e621d3bd15d2eabff241592072
Damian Salwa <d.salwa000@gmail.com>
2026-06-12 12:17:30 +0200

    next step

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,10 @@
 from argparse import ArgumentParser
-from typing import TypeAlias
 from llm_sdk import Small_LLM_Model
 import json
+import numpy as np
 
-Paths: TypeAlias = tuple[str, str, str]
-Input: TypeAlias = dict[str, str]
+Paths = tuple[str, str, str]
+Input = dict[str, str]
 
 
 def parser() -> Paths:
@@ -19,30 +19,49 @@ def parser() -> Paths:
     return (args.functions_definition, args.input, args.output)
 
 
-def print_data(description: str, data: list[Input]) -> None:
+def print_input(description: str, data: list[Input]) -> None:
     print(description)
     for log in data:
-        print(log)
+        print(log['prompt'])
     print()
 
 
+def print_functions(description: str, data: list[Input]) -> None:
+    print(description)
+    for log in data:
+        print(log["name"])
+        print(log["description"])
+        print()
+
+
 if __name__ == "__main__":
+    llm_model = Small_LLM_Model()
     definitions_path, input_path, output_path = parser()
+    vocab_path = llm_model.get_path_to_vocab_file()
+
+    with open(input_path, "r") as file:
+        input_data = json.load(file)
 
     with open(definitions_path, "r") as file:
         definitions = json.load(file)
 
-    with open(input_path, "r") as file:
-        input = json.load(file)
+    with open(vocab_path, "r") as file:
+        vocab = json.load(file)
 
-    llm_model = Small_LLM_Model()
+    print_input("Prompts", input_data)
+    print_functions("Prompts", definitions)
 
-    first = input[0]['prompt']
+    MAX_NEW_TOKENS = 50
+    i = 0
+    prompt = input_data[3]['prompt']
+    ids = llm_model.encode(prompt).tolist()[0]
 
-    # Aktualnie trzeba stworzyć maszynę stanu FSM Finite State Machine
+    print(f"Prompt: {prompt}")
+    while i < MAX_NEW_TOKENS:
+        logits = llm_model.get_logits_from_input_ids(ids)
+        next_token_id = int(np.argmax(logits))
+        ids.append(next_token_id)
+        i += 1
 
-    print("First")
-    print(llm_model.encode(first))
-
-    print_data("Definitions", definitions)
-    print_data("Input", input)
+    decoded = llm_model.decode(ids)
+    print(f"\nFinal: {decoded}")
