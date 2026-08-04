abaf629d08f301193284860787a88f4e1e6fe2b0
Damian Salwa <d.salwa000@gmail.com>
2026-08-04 22:01:23 +0200

    refactor

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,6 @@
 import json
 from argparse import ArgumentParser
+from typing import NamedTuple
 
 from numpy import argmax, array, full, inf
 
@@ -7,6 +8,14 @@ from llm_sdk import Small_LLM_Model
 
 Paths = tuple[str, str, str]
 Input = dict[str, str]
+StandardDict = dict[str, str]
+
+
+class VocabularyData(NamedTuple):
+    input_data: StandardDict
+    functions_def: StandardDict
+    output: StandardDict
+    vocab: dict[str, int]
 
 
 def build_final_prompt(user_prompt: str, functions_def: list[dict]) -> str:
@@ -39,6 +48,89 @@ def build_vocab_id_str(vocab: dict[str, int]) -> dict[int, str]:
     return {token_id: tok.replace("Ġ", " ") for tok, token_id in vocab.items()}
 
 
+def execute_prompt(
+    prompt: str,
+    functions_def: StandardDict,
+    llm_model: Small_LLM_Model,
+    vocab: dict[str, int]
+) -> None:
+    # Testing
+    reverse_vocab = build_vocab_id_str(vocab)
+
+    prompt_ids: list[int] = llm_model.encode(prompt).tolist()[0]
+    final_prompt: str = build_final_prompt(prompt, functions_def)
+    final_prompt_ids: list[int] = llm_model.encode(final_prompt).tolist()[0]
+
+    functions_token_ids: list[list[int]] = [
+        llm_model.encode(definition['name']).tolist()[0][1:]
+        for definition in functions_def
+    ]
+    max_function_length = max(len(f) for f in functions_token_ids)
+
+    function_token_groups: list[list[int]] = [
+        [] for _ in range(max_function_length)
+    ]
+
+    for function_ids in functions_token_ids:
+        function_token_groups[0].append(function_ids[0])
+
+    function_name_ids: list[int] = []
+    for position in range(len(function_token_groups)):
+        next_token_id: int = generate_next_token_id(
+            final_prompt_ids,
+            function_token_groups[position]
+        )
+
+        final_prompt_ids.append(next_token_id)
+        function_name_ids.append(next_token_id)
+
+        for index, function_ids in enumerate(functions_token_ids):
+            if (
+                position + 1 < len(function_ids)
+                and next_token_id == function_ids[position]
+            ):
+                token_id = function_ids[position + 1]
+                function_token_groups[position + 1].append(token_id)
+
+        response: str = llm_model.decode(final_prompt_ids).splitlines()[-1]
+
+        if (
+            position + 1 >= len(function_token_groups)
+            or function_token_groups[position + 1] == []
+        ):
+            break
+
+    function_arg: list[str] = extract_functions_arguments(
+        function_name_ids,
+        llm_model
+    )
+
+    force_text(final_prompt_ids, f'", "parameters": {{ "{function_arg[0]}": "')
+
+    next_token_id: int = generate_next_token_id(
+        final_prompt_ids,
+        prompt_ids
+    )
+
+    final_prompt_ids.append(next_token_id)
+
+    if len(function_arg) == 1:
+        force_text(final_prompt_ids, '" }}')
+    else:
+        force_text(final_prompt_ids, f'", {function_arg[1]}: "')
+
+        next_token_id: int = generate_next_token_id(
+            final_prompt_ids,
+            prompt_ids
+        )
+        final_prompt_ids.append(next_token_id)
+
+        force_text(final_prompt_ids, '" }')
+
+    response: str = llm_model.decode(final_prompt_ids).splitlines()[-1]
+    print(response)
+
+
 def force_text(ids: list[int], text: str) -> list[int]:
     ids.extend(llm_model.encode(text)[0])
 
@@ -53,6 +145,16 @@ def mask_logits(loits: list[float], allowed_ids: list[int]) -> list[int]:
     return masked.tolist()
 
 
+def generate_next_token_id(id_list: list[int], allowed_ids: list[int]) -> int:
+    logits: list[float] = llm_model.get_logits_from_input_ids(
+        id_list
+    )
+    masked_logits: list[int] = mask_logits(
+        logits, allowed_ids
+    )
+    return int(argmax(masked_logits))
+
+
 def parser() -> Paths:
     arg_parser = ArgumentParser(description="Call Me Maybe")
 
@@ -65,44 +167,23 @@ def parser() -> Paths:
     return (args.functions_definition, args.input, args.output)
 
 
-def print_input(description: str, data: list[Input]) -> None:
-    print(description)
-    for log in data:
-        print(log['prompt'])
-    print()
+def extract_functions_arguments(
+    function_name_ids: list[int],
+    llm_model: Small_LLM_Model
+) -> list[str]:
+    function_arg: list[str] = []
+    function_name = "fn" + llm_model.decode(function_name_ids)
+
+    for function_def in functions_def:
+        if function_def["name"] == function_name:
+            function_arg = [key for key in function_def['parameters']]
+            break
+
+    return function_arg
 
 
-def print_functions(description: str, data: list[Input]) -> None:
-    print(description)
-    for log in data:
-        print(log["name"])
-        print(log["description"])
-        print()
-
-
-def execute_prompts(input_data: list[str], functions_def: list[dict]):
-    for input in input_data:
-        MAX_NEW_TOKENS = 40
-        i = 0
-        user_prompt = input['prompt']
-
-        final_prompt = build_final_prompt(user_prompt, functions_def)
-        ids = llm_model.encode(final_prompt).tolist()[0]
-
-        while i < MAX_NEW_TOKENS:
-            logits = llm_model.get_logits_from_input_ids(ids)
-            next_token_id = int(argmax(logits))
-            ids.append(next_token_id)
-            i += 1
-
-        decoded = llm_model.decode(ids)
-        print(f"\nFinal: {decoded}")
-
-
-if __name__ == "__main__":
-    llm_model = Small_LLM_Model()
+def read_files(vocab_path: str) -> VocabularyData:
     definitions_path, input_path, output_path = parser()
-    vocab_path = llm_model.get_path_to_vocab_file()
 
     with open(input_path, "r") as file:
         input_data = json.load(file)
@@ -113,42 +194,18 @@ if __name__ == "__main__":
     with open(vocab_path, "r") as file:
         vocab = json.load(file)
 
-    reversed_vocab: dict[int, str] = build_vocab_id_str(vocab)
-    prompt: str = input_data[0]["prompt"]
+    output: dict[str, str] = {}
 
-    final_prompt: str = build_final_prompt(prompt, functions_def)
-    response: str = final_prompt.splitlines()[-1]
-    response_ids: list[int] = llm_model.encode(response).tolist()[0]
-    prompt_ids: list[int] = llm_model.encode(final_prompt).tolist()[0]
-    last_tokon_index: int = len(prompt_ids) - 1
+    return (input_data, functions_def, output, vocab)
 
-    functions_ids: list[list[int]] = [
-        llm_model.encode(definition['name']).tolist()[0][1:]
-        for definition in functions_def
-    ]
-    max_len = max(len(f) for f in functions_ids)
 
-    sorted_by_position: list[list[int]] = [[] for _ in range(max_len)]
+if __name__ == "__main__":
+    llm_model = Small_LLM_Model()
+    vocab_path = llm_model.get_path_to_vocab_file()
 
-    for function_ids in functions_ids:
-        sorted_by_position[0].append(function_ids[0])
+    input_data, functions_def, output, vocab = read_files(vocab_path)
 
-    for position in range(len(sorted_by_position) - 1):
-        logits: list[float] = llm_model.get_logits_from_input_ids(prompt_ids)
-        masked_logits: list[int] = mask_logits(
-            logits, sorted_by_position[position]
-            )
-        next_token_id: int = int(argmax(masked_logits))
+    for input in input_data:
+        execute_prompt(input['prompt'], functions_def, llm_model, vocab)
 
-        prompt_ids.append(next_token_id)
-        response_ids.append(next_token_id)
-
-        for index, function_ids in enumerate(functions_ids):
-            if (
-                position + 1 < len(function_ids)
-                and next_token_id == function_ids[position]
-            ):
-                token_id = function_ids[position + 1]
-                sorted_by_position[position + 1].append(token_id)
-
-    print(llm_model.decode(response_ids))
+    # execute_prompt(input_data[-1]['prompt'], functions_def, llm_model, vocab)
